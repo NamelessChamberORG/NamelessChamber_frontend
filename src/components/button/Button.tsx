f3e2c4ae5f8c0faf5d4eeae9355a45fb05f7fb08
Jiyun <jiyunk920@gmail.com>
2025-08-04 10:29:06 +0900

    💄  style: adjust button style (#4)

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,14 +1,27 @@
 import classes from "./Button.module.css";
-
 import type { ComponentPropsWithoutRef, ReactNode } from "react";
 
 type ButtonProps = ComponentPropsWithoutRef<"button"> & {
   children: ReactNode;
+  alwaysHoverStyle?: boolean; // ← 이게 true면 hover 스타일 항상 적용
 };
 
-const Button = ({ children, ...props }: ButtonProps) => {
+const Button = ({
+  children,
+  alwaysHoverStyle,
+  className = "",
+  ...props
+}: ButtonProps) => {
+  const combinedClassName = [
+    classes.button,
+    alwaysHoverStyle && classes["hover-style"], // hover 효과를 항상 주는 경우
+    className,
+  ]
+    .filter(Boolean)
+    .join(" ");
+
   return (
-    <button {...props} className={classes.button}>
+    <button {...props} className={combinedClassName}>
       {children}
     </button>
   );
