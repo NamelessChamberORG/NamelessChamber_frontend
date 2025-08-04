cb45a2872b68b5f22d6a38d22973db6336320178
Jiyun <jiyunk920@gmail.com>
2025-08-04 15:22:13 +0900

    ✨  feat: add link to logo in header (#4)

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 import Button from "../button/Button";
 import Logo from "../logo/Logo";
 import classes from "./Header.module.css";
@@ -5,7 +6,9 @@ import classes from "./Header.module.css";
 const Header = () => {
   return (
     <header className={classes.header}>
-      <Logo />
+      <Link to="/" className={classes.logoLink}>
+        <Logo />
+      </Link>
       <Button alwaysHoverStyle>로그인하기</Button>
     </header>
   );
