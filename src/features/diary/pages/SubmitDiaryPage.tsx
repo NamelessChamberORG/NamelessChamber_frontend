2a000ef4082b01d42092a6b99b622cf7b9731d55
Jiyun <jiyunk920@gmail.com>
2025-08-14 15:57:52 +0900

    💄  style: apply design change to the page (#6)

--- src/features/diary/pages/SubmitDiaryPage.tsx
+++ src/features/diary/pages/SubmitDiaryPage.tsx
@@ -1,12 +1,14 @@
-import Paragraph from "../../../components/paragraph/Paragraph";
 import CardList from "../components/card/CardList";
 import classes from "./SubmitDiaryPage.module.css";
 
 function SubmitDiaryPage() {
   return (
     <div className={classes.submit}>
-      <Paragraph>작성해주신 마음은 소중히 보관하겠습니다.</Paragraph>
-      <Paragraph>무명보관소</Paragraph>
+      <img
+        src="/logo_kr.png"
+        alt="nameless_chamber_logo"
+        style={{ width: "5rem" }}
+      ></img>
       <CardList />
     </div>
   );
