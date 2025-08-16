13de7eafe93412ebfc9501b3cb1ec84f71ca2691
Jiyun <jiyunk920@gmail.com>
2025-08-16 17:43:14 +0900

    💄  style: modify style of detail page (#17)

--- src/features/diary/pages/DiaryDetailPage.tsx
+++ src/features/diary/pages/DiaryDetailPage.tsx
@@ -2,16 +2,22 @@ import FadeInOnView from "../components/FadeInOnView";
 import classes from "./DiaryDetailPage.module.css";
 import mockDiaryDetail from "../../../mock/mockDiaryDetail";
 import Paragraph from "../../../components/paragraph/Paragraph";
+import { useMemo } from "react";
 
 function DiaryDetailPage() {
+  const paragraphs = useMemo(
+    () => mockDiaryDetail.content.trim().split(/\n{2,}/g),
+    []
+  );
+
   return (
     <section className={`${classes.diary} ${classes.detail}`}>
-      <Paragraph>곰곰히 시간을 들여서 다른 고민을 읽어보세요</Paragraph>
+      <Paragraph>곰곰이 시간을 들여서 다른 고민을 읽어보세요</Paragraph>
 
       <div className={classes.lines}>
-        {mockDiaryDetail.content.split("\n").map((line, index) => (
-          <FadeInOnView key={index} className={classes.content} once={true}>
-            {line}
+        {paragraphs.map((p, i) => (
+          <FadeInOnView key={i} className={classes.paragraph} once>
+            {p}
           </FadeInOnView>
         ))}
       </div>
