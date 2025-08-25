ebd6c39eb9e240a92aaf4373854725981fa262ca
Jiyun <jiyunk920@gmail.com>
2025-08-25 09:26:39 +0900

    ✨  feat: add closing section layout in detail page (#44)

--- src/features/diary/pages/DiaryDetailPage.tsx
+++ src/features/diary/pages/DiaryDetailPage.tsx
@@ -4,8 +4,9 @@ import Paragraph from "../../../components/paragraph/Paragraph";
 import classes from "./DiaryDetailPage.module.css";
 import { useMemo } from "react";
 import { useDiary } from "../hooks/useDiary";
-import { useParams, Navigate } from "react-router";
+import { useParams, Navigate, Link } from "react-router";
 import { formatDiaryTime } from "../../../lib/diary/formatDiaryTime";
+import Button from "../../../components/button/Button";
 
 function DetailContent({
   content,
@@ -35,11 +36,22 @@ function DetailContent({
             <div key={i} className={classes.paragraphSkeleton} />
           ))
         ) : paragraphs.length > 0 ? (
-          paragraphs.map((p, i) => (
-            <FadeInOnView key={i} className={classes.paragraph} once>
-              {p}
-            </FadeInOnView>
-          ))
+          <>
+            {paragraphs.map((p, i) => (
+              <FadeInOnView key={i} className={classes.paragraph} once>
+                {p}
+              </FadeInOnView>
+            ))}
+            <div className={classes.closing}>
+              <div>
+                <Paragraph>또 하나의 고백을 만나기 위해,</Paragraph>
+                <Paragraph>당신의 이야기를 한 번 더 흘려보내주세요</Paragraph>
+              </div>
+              <Link to="/diary/new" className={classes.link}>
+                <Button>한 번 더 흘려 보내기</Button>
+              </Link>
+            </div>
+          </>
         ) : (
           <div className={classes.empty} role="status" aria-live="polite">
             아직 내용이 없습니다.
