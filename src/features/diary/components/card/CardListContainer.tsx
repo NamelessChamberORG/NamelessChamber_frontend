2431bb870c906c7bb29fc536eae55b6d3beb96bf
Jiyun <jiyunk920@gmail.com>
2025-08-25 11:22:22 +0900

    ✨  feat: apply StoryPrompt component to diary list page (#44)

---
+++ src/features/diary/components/card/CardListContainer.tsx
@@ -0,0 +1,51 @@
+import { useNavigate } from "react-router-dom";
+import classes from "./CardList.module.css";
+import CardSkeleton from "./CardSkeleton";
+import CardList from "./CardList";
+import type { DiaryPreview } from "../../types/types";
+import StoryPrompt from "../storyPrompt/StoryPrompt";
+
+type Props = {
+  diaries: DiaryPreview[];
+  isLoading: boolean;
+  isEmpty: boolean;
+};
+
+const CardListContainer = ({ diaries, isLoading, isEmpty }: Props) => {
+  const navigate = useNavigate();
+
+  if (isLoading) {
+    return (
+      <ul className={classes.cardList}>
+        {Array.from({ length: 6 }).map((_, i) => (
+          <CardSkeleton key={i} />
+        ))}
+      </ul>
+    );
+  }
+
+  if (isEmpty) {
+    return (
+      <ul className={classes.cardList}>
+        <li className={classes.empty}>아직 등록된 글이 없어요.</li>
+      </ul>
+    );
+  }
+
+  return (
+    <>
+      <StoryPrompt
+        lines={["당신의 이야기를", "한 번 더 흘려보내주세요"]}
+        to="/diary/new"
+      />
+      <ul className={classes.cardList}>
+        <CardList
+          diaries={diaries}
+          onClickCard={(id) => navigate(`/diary/${id}`)}
+        />
+      </ul>
+    </>
+  );
+};
+
+export default CardListContainer;
--- src/features/diary/pages/DiaryListPage.tsx
+++ src/features/diary/pages/DiaryListPage.tsx
@@ -1,5 +1,5 @@
 import QueryBoundary from "../../../components/status/QueryBoundary";
-import CardList from "../components/card/CardList";
+import CardListContainer from "../components/card/CardListContainer";
 import { useDiaries } from "../hooks/useDiaries";
 import classes from "./DiaryListPage.module.css";
 
@@ -17,11 +17,6 @@ function DiaryListPage() {
 
   return (
     <div className={classes.submit}>
-      <img
-        src="/logo_kr.png"
-        alt="nameless_chamber_logo"
-        style={{ width: "5rem" }}
-      />
       <QueryBoundary
         isError={isError}
         error={error}
@@ -30,7 +25,11 @@ function DiaryListPage() {
         isEmpty={isEmpty}
         variant="list"
       >
-        <CardList diaries={diaries} isLoading={isFetching} />
+        <CardListContainer
+          diaries={diaries}
+          isLoading={isFetching}
+          isEmpty={isEmpty}
+        />
       </QueryBoundary>
     </div>
   );
