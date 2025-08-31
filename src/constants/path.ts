e6e2fddc1e7107060deb6bf94b23b1025c69317a
Jiyun <jiyunk920@gmail.com>
2025-08-31 21:50:08 +0900

    ✨  feat: add `v` prefix to diary detail route to avoid conflict with list route (#63)

--- src/constants/path.ts
+++ src/constants/path.ts
@@ -3,10 +3,10 @@ export const PATHS = {
   DIARY_LIST: "/diary/:type",
   DIARY_SUBMIT: "/diary/submit/:type",
   DIARY_NEW: "/diary/new/:type",
-  DIARY_DETAIL: "/diary/:id",
+  DIARY_DETAIL: "/diary/v/:id",
 
   DIARY_LIST_TYPE: (type: "short" | "long") => `/diary/${type}`,
   DIARY_SUBMIT_TYPE: (type: "short" | "long") => `/diary/submit/${type}`,
-  DIARY_DETAIL_ID: (id: string) => `/diary/${id}`,
+  DIARY_DETAIL_ID: (id: string) => `/diary/v/${id}`,
   DIARY_NEW_TYPE: (type: string) => `/diary/new/${type}`,
 } as const;
