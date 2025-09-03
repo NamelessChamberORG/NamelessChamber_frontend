42f60bd4180f2fa79ded0eab7a8768aba48ad15a
Jiyun <jiyunk920@gmail.com>
2025-09-04 08:47:25 +0900

    ✨  feat: add a link to login page in header (#65)

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,22 +2,17 @@ import { Link } from "react-router";
 import Button from "../button/Button";
 import Logo from "../logo/Logo";
 import classes from "./Header.module.css";
-import { useToast } from "../../contexts/ToastContext";
 import { PATHS } from "../../constants/path";
 
 const Header = () => {
-  const { showToast } = useToast();
   return (
     <header className={classes.header}>
       <Link to={PATHS.HOME} className={classes.logoLink}>
         <Logo />
       </Link>
-      <Button
-        alwaysHoverStyle
-        onClick={() => showToast("아직 준비되지 않은 기능입니다", "info")}
-      >
-        로그인하기
-      </Button>
+      <Link to={PATHS.LOGIN}>
+        <Button alwaysHoverStyle>로그인하기</Button>
+      </Link>
     </header>
   );
 };
