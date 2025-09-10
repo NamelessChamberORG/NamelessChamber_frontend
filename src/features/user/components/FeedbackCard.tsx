3e42eb65c282c5cc0154ed703a34440b8c775bda
Jiyun <jiyunk920@gmail.com>
2025-09-10 19:45:43 +0900

    ✨  feat: add FeedbackCard component (#74)

---
+++ src/features/user/components/FeedbackCard.tsx
@@ -0,0 +1,16 @@
+import Button from "../../../components/button/Button";
+import classes from "./FeedbackCard.module.css";
+
+const FeedbackCard = () => {
+  return (
+    <div className={classes.feedbackCard}>
+      <div>
+        뭐든지 피드백 주세요.
+        <br />더 좋은 서비스로 보답할게요!
+      </div>
+      <Button>&gt;</Button>
+    </div>
+  );
+};
+
+export default FeedbackCard;
