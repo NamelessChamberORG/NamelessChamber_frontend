156f96fc14a58efadc411ae6d09db0008f7ae807
Jiyun <jiyunk920@gmail.com>
2025-09-11 22:42:10 +0900

    ✨  feat: connect /posts/me and /read-history api to UI (#74)

--- src/features/user/pages/ProfilePage.tsx
+++ src/features/user/pages/ProfilePage.tsx
@@ -7,19 +7,43 @@ import { useUserMe } from "../hooks/useUser";
 import classes from "./ProfilePage.module.css";
 import { PATHS } from "../../../constants/path";
 import { clearAuth } from "../../auth/api/tokenStore";
+import DiaryTabs from "../components/DiaryTabs";
+import { useReadDiaries } from "../hooks/useReadDiaries";
+import { useMemo, useState } from "react";
+import CardListContainer from "../../diary/components/card/CardListContainer";
+import { useWrittenDiaries } from "../hooks/useWrittenDiaries";
 
 function ProfilePage() {
+  const [currentTab, setCurrentTab] = useState<"written" | "read">("written");
   const { data: me, isLoading: isMeLoading } = useUserMe();
   const navigate = useNavigate();
+
+  const { data: readData, isLoading: isReadLoading } = useReadDiaries(
+    currentTab === "read"
+  );
+
+  const { data: writtenData, isLoading: isWrittenLoading } =
+    useWrittenDiaries();
+
+  const diaries = useMemo(() => {
+    if (currentTab === "read") return readData?.posts ?? [];
+    return writtenData ?? [];
+  }, [currentTab, readData, writtenData]);
 
   if (isMeLoading) {
     return <div>Loading...</div>;
   }
 
+  const isLoading =
+    isMeLoading || (currentTab === "read" ? isReadLoading : isWrittenLoading);
+
+  const isEmpty = !isLoading && diaries.length === 0;
+
   function handleLogout() {
     clearAuth();
     navigate(PATHS.HOME);
   }
+
   return (
     <section className={classes.profile}>
       <div className={classes.profileSection}>
@@ -30,6 +54,24 @@ function ProfilePage() {
         <button className={classes.logout} onClick={handleLogout}>
           로그아웃
         </button>
+      </div>
+
+      <div className={classes.listSection}>
+        <DiaryTabs onChange={(id) => setCurrentTab(id as "written" | "read")} />
+
+        <CardListContainer
+          diaries={diaries}
+          isLoading={isLoading}
+          isEmpty={isEmpty}
+          coin={me?.coin ?? 0}
+          type="daily"
+          emptyMessage={
+            currentTab === "read"
+              ? "아직 열람한 글이 없어요."
+              : "아직 작성한 글이 없어요."
+          }
+          interactionMode="direct"
+        />
       </div>
     </section>
   );
