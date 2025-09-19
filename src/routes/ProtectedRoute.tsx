98db8d72a840447f6da1f28ddb7ca075fc42e474
Jiyun <jiyunk920@gmail.com>
2025-09-19 16:58:29 +0900

    ♻️  refactor: use isAuthenticatedUser function for maintability (#101)

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,10 +1,9 @@
 import { Navigate, Outlet } from "react-router-dom";
-import { getCurrentIdentity } from "../features/auth/api/tokenStore";
+import { isAuthenticatedUser } from "../features/auth/api/tokenStore";
 import { PATHS } from "../constants/path";
 
 const ProtectedRoute = () => {
-  const identity = getCurrentIdentity();
-  const ok = !!identity && identity.role !== "ANONYMOUS";
+  const ok = isAuthenticatedUser();
 
   return ok ? <Outlet /> : <Navigate to={PATHS.LOGIN} replace />;
 };
