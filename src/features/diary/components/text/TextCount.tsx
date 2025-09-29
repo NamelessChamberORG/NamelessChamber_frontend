f5ae97d483b87887696fc82700f578cfec0d60bf
Jiyun <jiyunk920@gmail.com>
2025-09-29 17:42:05 +0900

    ✨  feat: add reqLength prop and show remaining/exceeded chars (#120)

--- src/features/diary/components/text/TextCount.tsx
+++ src/features/diary/components/text/TextCount.tsx
@@ -2,10 +2,16 @@ import classes from "./TextCount.module.css";
 
 type TextCountProps = {
   count: number;
+  reqLength: number;
 };
 
-const TextCount = ({ count }: TextCountProps) => {
-  return <div className={classes.textCount}>{count}자</div>;
+const TextCount = ({ count, reqLength }: TextCountProps) => {
+  const text =
+    count < reqLength
+      ? `${reqLength - count}자 남음`
+      : `${count - reqLength}자 넘음`;
+
+  return <div className={classes.textCount}>{text}</div>;
 };
 
 export default TextCount;
