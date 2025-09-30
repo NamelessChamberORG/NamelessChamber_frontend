e7719bb014789217b7d9ee55405d8293cb35a86d
Jiyun <jiyunk920@gmail.com>
2025-09-30 22:12:02 +0900

    ♻️  refactor: allow div props in CoinInfo component (#124)

--- src/features/user/components/CoinInfo.tsx
+++ src/features/user/components/CoinInfo.tsx
@@ -1,12 +1,13 @@
+import { type ComponentPropsWithoutRef } from "react";
 import classes from "./CoinInfo.module.css";
 
 type CoinInfoProps = {
   coin: number;
-};
+} & ComponentPropsWithoutRef<"div">;
 
-const CoinInfo = ({ coin }: CoinInfoProps) => {
+const CoinInfo = ({ coin, ...rest }: CoinInfoProps) => {
   return (
-    <div className={classes.coin}>
+    <div className={classes.coin} {...rest}>
       <p>열람권</p>
       <p>{coin}개</p>
     </div>
