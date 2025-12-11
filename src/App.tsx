be5d836d9dd0e3528b80008fc24194374ee2861d
respectwo2 <respectwo@naver.com>
2025-12-11 11:21:06 +0900

    fix: 마이페이지 라우터 이슈 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { RouterProvider } from "react-router";
+import { RouterProvider } from "react-router-dom";
 import router from "./routes";
 import { useEffect } from "react";
 import { attachGlobalFullscreenListenersOnce } from "./lib/fullscreen/fullscreenStore";
